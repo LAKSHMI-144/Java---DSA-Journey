464642bc88c8ab53a2af588f2ec49fec8bf4e0d4
lakshmi r <llakshmir895@gmail.com>
2026-01-30 21:46:11 +0530

    Datasizerange.java

--- DAY2/Datasizerange.java
+++ DAY2/Datasizerange.java
@@ -1,1 +1,35 @@
+class DataSizeRange {
+    public static void main(String[] args) {
 
+        System.out.println("Byte Max Value = " + Byte.MAX_VALUE);
+        System.out.println("Byte Min Value = " + Byte.MIN_VALUE);
+        System.out.println("Byte Size = " + Byte.BYTES + " bytes");
+
+        System.out.println("Short Max Value = " + Short.MAX_VALUE);
+        System.out.println("Short Min Value = " + Short.MIN_VALUE);
+        System.out.println("Short Size = " + Short.BYTES + " bytes");
+
+        System.out.println("Integer Max Value = " + Integer.MAX_VALUE);
+        System.out.println("Integer Min Value = " + Integer.MIN_VALUE);
+        System.out.println("Integer Size = " + Integer.BYTES + " bytes");
+
+        System.out.println("Long Max Value = " + Long.MAX_VALUE);
+        System.out.println("Long Min Value = " + Long.MIN_VALUE);
+        System.out.println("Long Size = " + Long.BYTES + " bytes");
+
+        System.out.println("Float Max Value = " + Float.MAX_VALUE);
+        System.out.println("Float Min Value = " + Float.MIN_VALUE);
+        System.out.println("Float Size = " + Float.BYTES + " bytes");
+
+        System.out.println("Double Max Value = " + Double.MAX_VALUE);
+        System.out.println("Double Min Value = " + Double.MIN_VALUE);
+        System.out.println("Double Size = " + Double.BYTES + " bytes");
+
+        System.out.println("Character Max Value = " + (int) Character.MAX_VALUE);
+        System.out.println("Character Min Value = " + (int) Character.MIN_VALUE);
+        System.out.println("Character Size = " + Character.BYTES + " bytes");
+
+        System.out.println("Boolean Values = true or false");
+        System.out.println("Boolean Size = Not defined");
+    }
+}
